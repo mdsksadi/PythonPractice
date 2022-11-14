9ed1ed7c5877796a7f09c60e0873c5e8f136ad13
mdsksadi <sadiewu12345@gmail.com>
2022-11-14 23:36:13 +0100

    Variables and Data Types

---
+++ 003_VariablesAndDataTypes.py
@@ -0,0 +1,14 @@
+name = "Shekh Sadi"
+age = 26
+height = 5.6 
+
+
+print("My name is "+name)   #We need to use "+" for string
+print("I am ",age,"years old")  #We need to use "," for int
+print("My height is",height)    #We need to use "," for float
+
+#We can also use "type" function for getting the variable type
+
+print(type(name))   
+print(type(age))
+print(type(height))
