f8a00303608b545d990be496bce916e40ed1d243
mdsksadi <sadiewu12345@gmail.com>
2022-11-14 23:21:47 +0100

    Backslash Charector/Scape Sequence and Comment

---
+++ 002_BackslashCharectorAndComment.py
@@ -0,0 +1,26 @@
+#We use comment as a side note
+
+'''
+This is
+a multiple 
+line
+comment
+'''
+
+print("MD Shekh Sadi")
+print("I am 26 years old")
+
+#Backslash Charector
+
+print("I am MD Shekh Sadi \nI am 26 years old")    #Backslash charector must be inside of double quatation
+print("My name is \t Shekh Sadi")   #It will work as a tab
+print("\"MD Shekh Sadi\"")  #Backslash for double quatation
+
+'''
+Some Backslash character:
+\n = new line
+\t = tab
+\" = double quatation
+\' = Single quatation
+These are also called scape sequence
+'''
