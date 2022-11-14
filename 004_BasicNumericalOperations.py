4e28208386fb4ac0f459401ec5c7a1e1c9832468
mdsksadi <sadiewu12345@gmail.com>
2022-11-14 23:46:41 +0100

    Basic Numerical Operations

---
+++ 004_BasicNumericalOperations.py
@@ -0,0 +1,31 @@
+'''
+Basic Numerical Charectors:
++ = Plus
+- = Minus
+* = Multiply
+/ = Devide
+% = Modulas
+// = Floor
+** = Exponentital
+'''
+
+print(11+4)
+print(11-4)
+print(11*4)
+print(11/4)
+print(11%4)
+print(11//4)
+print(11**4)
+
+#By using variable:
+
+a = 11
+b = 4 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b) #We can also use floor function to get the floor
+print(a**b)
