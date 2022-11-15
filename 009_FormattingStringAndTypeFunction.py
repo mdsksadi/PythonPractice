129fef3ad219049cb06337adf4cbb2d99539b40a
mdsksadi <sadiewu12345@gmail.com>
2022-11-15 16:30:56 +0100

    Formatting String and Type Function

---
+++ 009_FormattingStringAndTypeFunction.py
@@ -0,0 +1,18 @@
+num1 = 20
+print(type(num1))
+
+num2 = 20.5
+print(type(num2))
+
+name = "MD Shekh sadi"
+print(type(name))
+
+print("The sum is: ",num1+num2)
+
+#String Formating
+
+print(f"{num1}+{num2}={num1+num2}")
+print("The sum is: ",f"{num1}+{num2}={num1+num2}")
+
+print("Shekh Sadi", end = " ")  #here end=" " will show a space instead of new line
+print("is doing his internship in BOSCH")
