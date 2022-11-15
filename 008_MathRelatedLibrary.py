ffac533aa312773017014ee83793140c176f5efe
mdsksadi <sadiewu12345@gmail.com>
2022-11-15 16:14:34 +0100

    Math Related Basic Library

---
+++ 008_MathRelatedLibrary.py
@@ -0,0 +1,16 @@
+'''
+Functions are two types:
+    1. Library function
+    2. User define function
+'''
+from math import * #It means from math import all
+
+print(max(20,10))   #The result is 20
+print(min(20,10))   #The result is 10
+print(abs(-4))      #The result is 4
+print(pow(2,3))     #The result is 8
+print(sqrt(25))     #The result is 5
+print(round(5.64))  #The result is 6
+print(round(5.43))  #The result is 5
+print(ceil(3.23))   #The result is 4
+print(floor(3.98))  #The result is 3
