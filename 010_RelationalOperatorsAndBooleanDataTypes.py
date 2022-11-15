c6f100e3f4ee3e0f236c432da729591ec8ea561d
mdsksadi <sadiewu12345@gmail.com>
2022-11-15 16:50:13 +0100

    Relational Operator and Bookean Data Types

---
+++ 010_RelationalOperatorsAndBooleanDataTypes.py
@@ -0,0 +1,21 @@
+'''
+Relational Operators:   #These are also called comparisonal operator
+
+Greater than:  >
+Less than:  <
+Equal:  ==
+Less than and equal: <=
+Greater than and equal: >=
+Not equal:  !=
+
+The result of relational operators is boolian value. 1/0
+
+'''
+print(30>20)
+print(30<20)
+print(30==20)
+print(30>=20)
+print(30<=20)
+print(30!=20)
+print("hello"=="hello")
+print("hello"=="helow")
