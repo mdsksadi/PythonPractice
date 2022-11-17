96699b2af7cb0d24cf06bb3faa7b499075ba584e
mdsksadi <sadiewu12345@gmail.com>
2022-11-17 16:37:48 +0100

    While Loop

---
+++ 015_WhileLoop.py
@@ -0,0 +1,26 @@
+# print 0 to 100:
+
+i=0
+
+while i<=100:
+    print(i)
+    i=i+1
+print("End")
+
+#Print 0 to 100 odd number:
+
+j = 1
+
+while j<=100:
+    print(j)
+    j=j+2
+print("End this part")
+
+#Print 0 to 100 even number:
+
+k = 0
+
+while k<=100:
+    print(k)
+    k=k+2       # k+=2 will also do the same task.
+print("End this part")
