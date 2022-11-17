58c6c416f83314044e4657df31647798bfe6c785
mdsksadi <sadiewu12345@gmail.com>
2022-11-17 21:22:21 +0100

    Range

---
+++ 019_Range.py
@@ -0,0 +1,11 @@
+
+num1=list(range(10))    #rangle will generate 0 to 9. convert these 10 values into list and keep it in variable num1
+print(num1)
+
+print(num1[2])  #now we can use range as a list.
+
+num2=list(range(5,11))  #we can also specify the starting and ending of the range. It will show 5 to 10
+print(num2)
+
+num3=list(range(3,20,2))    #We can also specify the gap inbetween two values. here the gap is 2, starting pint 3, ending point 19
+print(num3)
