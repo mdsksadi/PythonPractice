e8d93de79b8f1accf097d9eead6a34c41a373106
mdsksadi <sadiewu12345@gmail.com>
2022-11-29 22:49:56 +0100

    List User Input Updated

--- 025_ListUserInput.py
+++ 025_ListUserInput.py
@@ -1,9 +1,7 @@
-'''
-GCD is Greatest Common Divisor
-
-Formula: 
-'''
-
+########################################
+#             MD Shekh Sadi            #
+#        sadiewu12345@gmail.com        #
+########################################
 
 #Take list input from user:
 
@@ -11,10 +9,49 @@ import math
 
 numbers = input("Enter your numbers with spaces: ")
 
-fNumbers = numbers.split()
+fNumbers1 = numbers.split()
 
-for x in range(len(fNumbers)):
+print("Input from user as char: ",fNumbers1)
+
+print("Input from user: ")
+for x in range(len(fNumbers1)):
     
-    fNumbers[x]=int(fNumbers[x])
+    fNumbers1[x]=int(fNumbers1[x])
     
-    print(fNumbers[x])
+    print(fNumbers1[x],"\n")
+
+fNumbers2 = numbers.split()
+sum = 0
+for y in fNumbers2:
+    sum = sum+int(y)
+print("The sum is: ",sum)
+
+digit = 0
+letter = 0
+word = 0
+
+inp= input("Enter your text: ")
+
+#Number of letter:
+
+for p1 in inp:
+    if p1==" ":
+        continue
+    letter=letter+1
+print("Total letter: ",letter)
+
+#Number of word:
+
+for p2 in inp:
+    if p2==" ":
+        word=word+1
+print("Total word: ",word+1)
+
+#Number of digit:
+
+for p3 in inp:
+    if p3>='0' and p3<='9':
+        digit=digit+1
+    else:
+        continue
+print("Total digit: ",digit)
